8f6fcc8c28c98fc1bceacf5740f6f968123d2725
Yashkumargoyal1 <yashkumargoyal.it26@jecrc.ac.in>
2024-09-25 10:51:24 +0000

    converstionofdatatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+/*{
+let score=33
+console.log(typeof score)
+
+// but i want to conert the number in the string
+let str=String(score)
+console.log(typeof str);
+console.log( str);
+}
+
+// second
+{
+    let name="yash";
+    console.log(typeof name);
+
+    // convert in num
+    let number=Number(name);
+    console.log(typeof number);
+    console.log(number)
+
+    
+
+}*/
+// do the investigation study
+{
+    let isloggedin=1; // how we can find that it's a bool value
+    console.log(typeof isloggedin);
+    // convert in the bool
+    let boolen=Boolean(isloggedin)
+    console.log(boolen);
+    
+    
+}
+
+ 
