ac32ae07e6e4a4e1265d64440c975467000ff959
Yashkumargoyal1 <yashkumargoyal.it26@jecrc.ac.in>
2024-09-25 10:12:15 +0000

    veriables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=5
+let accountEmail="yash.com"
+var accountPassword="13546"
+
+//accountId=9
+/*
+Prefer not to use var
+because of issue in blockscope and functional scope
+*/
+accountCity="jaipur"
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity])
