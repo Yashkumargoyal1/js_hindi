86a4e7dc6e836abb0fc96694e2c60b1dbde05fae
Yashkumargoyal1 <yashkumargoyal.it26@jecrc.ac.in>
2024-09-27 10:53:05 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,13 @@
+console.log(2>4); 
+console.log("2">1); // it gives true but it is not a good way to write
+
+console.log(null ==0);
+console.log(null >0);
+console.log(null >=0); // in this case compiler convert the null int 0
+
+console.log(undefined==0); // all gives false
+console.log(undefined>0);
+console.log(undefined<=0);
+
+
+
