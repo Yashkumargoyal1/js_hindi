78b7746263e2b146e315a221f30a10b078b2d4ff
Yashkumargoyal1 <yashkumargoyal.it26@jecrc.ac.in>
2024-09-27 10:29:12 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,14 +22,49 @@ console.log( str);
 
 }*/
 // do the investigation study
-{
-    let isloggedin=1; // how we can find that it's a bool value
+/*{
+    let isloggedin=1 // how we can find that it's a bool value
     console.log(typeof isloggedin);
     // convert in the bool
     let boolen=Boolean(isloggedin)
     console.log(boolen);
     
     
-}
+}*/
+/************************************************* operations */
+console.log(2+3);
+console.log(3-1)
+console.log(2*3);
+console.log(2**3)  // it is used to perform the power operation
+console.log(5/2)
+
+let num=8;
+num+=7
+console.log(num);
+
+let str="yash"
+let str2=" kumar"
+let str3=" goyal"
+str=str+str2+str3
+console.log(str);
+// but
+console.log("1"+2);//12
+console.log("1"+2+2);//122
+console.log(1+2+"3"); // 33
+
+console.log(true) // true
+console.log(+true)//1
+console.log("")// boolean
+console.log(+"");// 0
+
+
+
+
+
+
+
+
+
+
 
  
