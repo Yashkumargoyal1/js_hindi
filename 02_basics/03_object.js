7d644557362a2e908f2692f3e1f0a3ea046b4e10
Yashkumargoyal1 <yashkumargoyal.it26@jecrc.ac.in>
2024-11-11 15:55:52 +0000

    hi

---
+++ 02_basics/03_object.js
@@ -0,0 +1,52 @@
+// objects do tarah se bante hai
+// 1.singleton (Object.create())
+// 2.literals (const user={};)
+
+const user={
+    name: "yash",
+    age: 18,
+    gende :"male",
+    location :"jaipur",
+    email :"yash@microsoft.com"
+};
+// access
+// 1.st methos
+console.log(user.email);
+//2
+console.log(user["email"]);
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
+
+
